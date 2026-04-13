732bd4ed2be12d5d276c84605ae991600239de6c
Sean Molenaar <smillerdev@me.com>
2026-04-13 11:23:10 +0200

    Make sure appimage is executable

--- Library/Homebrew/cask/artifact/appimage.rb
+++ Library/Homebrew/cask/artifact/appimage.rb
@@ -10,6 +10,25 @@ module Cask
       def resolve_target(target)
         Pathname.new("#{config.appimagedir}/#{target}")
       end
+
+      sig {
+        override.params(
+          force:    T::Boolean,
+          adopt:    T::Boolean,
+          command:  T.class_of(SystemCommand),
+          _options: T.anything,
+        ).void
+      }
+      def link(force: false, adopt: false, command: SystemCommand, **_options)
+        super
+        return if source.executable?
+
+        if source.writable?
+          FileUtils.chmod "+x", source
+        else
+          command.run!("chmod", args: ["+x", source], sudo: true)
+        end
+      end
     end
   end
 end
